d4cdad1b4c23020c4050a69c0ae1a4b5b39cb7c3
jonathonlaylo <jonathon.laylo@gmail.com>
2017-03-15 20:43:07 -1000

    created the 4 states

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 import angular from 'angular';
 
 import '../style/app.css';
@@ -5,21 +6,13 @@ import '../style/app.css';
 let app = () => {
   return {
     template: require('./app.html'),
-    controller: 'AppCtrl',
     controllerAs: 'app'
-  }
+  };
 };
-
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
 
 const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+  .directive('app', app);
 
 export default MODULE_NAME;
